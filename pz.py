5c8b58c63256b13735edda99b3c58ed8b30a703f
Douknowccy <keddy406@hotmail.com>
2019-11-13 16:48:15 +0800

    count

--- pz.py
+++ pz.py
@@ -1,8 +1,11 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
 	num = input('請猜數字:\n')
 	num = int(num)
+	count += 1
+	print('這是你第',count,'幾次猜')
 	if num == r:
 		print('恭喜答對')
 		break
@@ -10,4 +13,3 @@ while True:
 		print('猜錯了 你猜的數字太大')
 	elif num < r:
 		print('猜錯了 你猜的數字太小')
-		
